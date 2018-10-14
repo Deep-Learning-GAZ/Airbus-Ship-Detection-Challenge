9c592e39f767546952782d9f57135ade9ac6aa50
matech <matech@sch.bme.hu>
2018-10-14 08:23:50 +0200

    Box parameters done
    by Zoli

---
+++ Utilities/shipDetection.py
@@ -0,0 +1,86 @@
+import numpy as np
+import math
+
+def shipDetection(matrix):
+# Definition of this function:  this function will help to find the ship on a greyscale image.
+
+# Input: matrix: it's a 2D array that is a greyscale image, so there are only 0-1 in the matrix, 1 is where the ship is.
+
+# Output:  centerx: the x coordinate of the center of the ship 
+#          centery: the y coordinate of the center of the ship  
+#          alfa: the orientation of the ship
+#          long_side: the length of the longer side of the ship
+#          short_side: the length of the shorter side of the ship
+
+    print("Original matrix:")
+    print(matrix)
+
+    # Find all of the ones in the matrix, we get 2 arrays
+    [rows,columns] = np.where(matrix==1)
+
+    #Now, we have to find the position of the 4 corners of the ship:
+    # It's important that now the rows are like the y coordinates, columns like the x coordinates
+
+    #Upside corner of the ship(we search the first row, where is a 1 ):
+    up_value = np.min(rows) 
+    position_up = np.where(rows==up_value) # the index in the array
+    column_up = columns[position_up]
+    row_up = rows[position_up]
+
+
+    #Right corner of the ship (we search the last column, where is a 1):
+    max_right = np.max(columns) 
+    position_right = np.where(columns==max_right) # the index in the array
+    column_right = columns[position_right] 
+    row_right = rows[position_right]
+
+    #Down corner of the ship (we search the last row, where is a 1):
+    max_down = np.max(rows) 
+    position_down = np.where(rows==max_down) # the index in the array
+    column_down = columns[position_down]
+    row_down = rows[position_down]
+
+    #Left corner of the ship (we search the first column, where is a 1):
+    min_left = np.min(columns) 
+    position_left = np.where(columns==min_left) # the index in the array
+    column_left = columns[position_left]
+    row_left = rows[position_left]
+
+    print ("Upper point of the ship is: ",row_up, column_up)
+    print ("Right point of the ship is: ",row_right, column_right)
+    print ("Down point of the ship is: ", row_down, column_down)
+    print ("Left point of the ship is: ",  row_left, column_left)
+
+    print(rows)
+    print(columns)
+
+    # Calculate the distence between the up and right corners of the ship
+    distance1 = math.sqrt((row_up-row_right)*(row_up-row_right) + (column_up-column_right)*(column_up-column_right))
+    print(distance1)
+
+    # Calculate the distence between the right and down corners of the ship
+    distance2 = math.sqrt((row_right-row_down)*(row_right-row_down) + (column_right-column_down)*(column_right-column_down))
+    print(distance2)
+
+    if distance1< distance2:
+        alfa= math.atan2((row_left-row_up), (column_left-column_up))  # maybe needs further development
+        centerx = (column_up+column_right)/2 + distance2/2*math.cos(alfa)
+        centery = (row_up+row_right)/2 + distance2/2*math.sin(alfa)
+        long_side= distance2
+        short_side= distance1
+        print("Alfa:",alfa*180/math.pi)
+        print ("centerx, centery", centerx,centery)
+    else:
+        alfa= math.atan2((row_left-row_up), (column_left-column_up)) + math.pi/2   # maybe needs further development
+        centerx = (column_down+column_right)/2 + distance1/2*math.cos(alfa)
+        centery = (row_down+row_right)/2 + distance1/2*math.sin(alfa)
+        long_side= distance1
+        short_side= distance2
+        print("Alfa:",alfa*180/math.pi)
+        print ("centerx, centery", centerx,centery)
+        
+    return centerx, centery, alfa, long_side,short_side
+# just for testing:
+#matrix = np.array([[ 0, 0, 0, 0 , 0, 0], [ 0,0,0,1,0,0], [0, 0, 1, 1, 1, 0 ], [0, 1, 1, 1, 0, 0 ], [0,0, 1, 0, 0, 0 ],[0, 0, 0, 0, 0, 0 ]])
+# [centerx, centery, alfa, long_side,short_side]=ship_detection(matrix)
+
